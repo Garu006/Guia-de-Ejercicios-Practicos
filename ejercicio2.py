dfd2ea5e8182e9211c520f7efeed269b11beaac3
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-02 23:12:24 -0600

    asignación de lo que se debe de hacer

---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+'''Se requiere que los estudiantes diseñen un módulo independiente que contenga 
+implementaciones  de algoritmos  de ordenamiento  simples  (bubble  sort). El 
+objetivo es  que,  a  partir de  una  función  principal, se  invoquen los  métodos del 
+módulo para ordenar una lista de números y demostrar la correcta separación de 
+responsabilidades, fomentando la modularidad y la reutilización del código.'''
+
---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+'''Crear una  clase Factura que simule el proceso de facturación de una  venta. Los 
+estudiantes deberán encapsular  los datos  internos de  la factura  (como  los 
+detalles de los productos, cantidades, precios y descuentos) y proveer métodos 
+para calcular el total de la venta, generar rep ortes simples y validar la integridad 
+de la información. Este ejercicio enfatiza la importancia de ocultar la 
+implementación interna y de diseñar interfaces claras y seguras  para la gestión 
+de transacciones comerciales.'''
+
