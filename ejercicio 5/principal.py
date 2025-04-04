0b613603ad11644fb86ada95facfd12fe13139ae
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-03 22:29:59 -0600

    viendo el código

--- ejercicio 5/principal.py
+++ ejercicio 5/principal.py
@@ -6,3 +6,16 @@ y aplicar descuentos especiales, demostrando el uso de la herencia y el
 polimorfismo para adaptar el  comportamiento de  los  objetos según  el  tipo  de 
 cliente.'''
 
+from clases import Cliente, ClienteVIP, Pedido
+
+def main():
+    print("\n--Registro de Clientes--")
+    id = int(input("\nIngrese el ID del cliente: "))
+    nombre = input("\nIngrese el nombre del cliente: ")
+    contacto = input("Ingrese el contacto del cliente: ")
+    tipo_cliente = input("El cliente es VIP o no?: ")
+    
+    if tipo_cliente == 'si' or tipo_cliente == 'sí':
+        cliente = ClienteVIP(id, nombre, contacto)
+    else:
+        cliente = Cliente(id, nombre, contacto)
