93b7077dde2ddd5bd1ec10ce9508b8b6d18ed985
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-04 21:47:50 -0600

    codigo funcional

--- ejercicio 6/principal.py
+++ ejercicio 6/principal.py
@@ -6,3 +6,37 @@ de la información. Este ejercicio enfatiza la importancia de ocultar la
 implementación interna y de diseñar interfaces claras y seguras  para la gestión 
 de transacciones comerciales.'''
 
+from clase_factura import Factura
+
+def main():
+    factura = Factura()
+    print("\n--Bienvenido al sistema de facturación--\n")
+    while True:
+        nombre = input("\nIngrese el nombre del producto: ")
+        
+        try:
+            cantidad = int(input("\nCantidad del producto: "))
+            precio = float(input("\nPrecio del producto($): "))
+            factura.agregar_producto(nombre, cantidad, precio)
+            print("\n✔ Producto agregado.")
+        except ValueError:
+            print("\nError. Inténtelo de nuevo.")
+            continue
+        
+        continuar = input("\n¿Desea agregar otro producto? (s/n): ")
+        if continuar not in ["si", "sí", "s"]:
+            break
+        
+    try:
+        descuento = float(input("\nIngrese el porcentaje de descuento que desea agregar (0-100):"))
+        factura.descuento(descuento)
+    except ValueError:
+        print("\nDescuento inválido. No se aplicará ninguno.")
+        
+    if factura.validador():
+        factura.reporte()
+    else:
+        print("\nEror: Hay productos con datos inválidos.")
+        
+if __name__ == '__main__':
+    main()
